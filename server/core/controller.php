8aee64506b7b3b1ad389b06b3ec8edd489d2ff60
LuongNhanKiet2020 <luongnhankiet2012@gmail.com>
2025-03-01 10:23:23 +0700

    error with section class client_section layout_padding-bottom in views/index.php not show

--- server/core/controller.php
+++ server/core/controller.php
@@ -1,27 +1,27 @@
-<?php 
+<?php
 
-Class Controller
+class Controller
 {
 
-	protected function view($view,$data = [])
+	protected function view($view, $data = [])
 	{
-		if(file_exists("../server/views/". $view .".php"))
- 		{
- 			require "../server/views/". $view .".php";
- 		}else{
- 			require "../server/views/404.php";
- 		}
+		$viewPath = __DIR__ . "/../views/" . $view . ".php";
+		if (file_exists($viewPath)) {
+
+			require $viewPath;
+		} else {
+			require "../server/views/404.php";
+		}
 	}
 
 	protected function loadModel($model)
 	{
-		if(file_exists("../server/models/". $model .".php"))
- 		{
- 			include "../server/models/". $model .".php";
- 			return $model = new $model();
- 		}
+		if (file_exists("../server/models/" . $model . ".php")) {
+			include "../server/models/" . $model . ".php";
+			return $model = new $model();
+		}
 
- 		return false;
+		return false;
 	}
 
 
